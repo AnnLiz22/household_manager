65bc84df924480c820a2bf45dde04d63d8c5dca6
AnnLiz22 <bielakam@gmail.com>
2024-07-13 21:50:57 +0200

    created entities

--- src/main/java/pl/coderslab/task/Task.java
+++ src/main/java/pl/coderslab/task/Task.java
@@ -1,4 +1,46 @@
 package pl.coderslab.task;
 
-public class Task {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+import pl.coderslab.category.Category;
+import pl.coderslab.user_task.UserTask;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "task")
+public class Task implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String taskName;
+
+    private String taskDescription;
+
+    @ManyToOne
+    private Category category;
+
+    private LocalDateTime createdOn;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dueDate;
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL)
+    private List<UserTask> usersTasks = new ArrayList<>();
+
+    @PrePersist
+    public void prePersist() {
+        createdOn = LocalDateTime.now();
+    }
 }
--- src/main/java/pl/coderslab/user_task/UserTask.java
+++ src/main/java/pl/coderslab/user_task/UserTask.java
@@ -1,29 +1,39 @@
-package pl.coderslab.comment;
+package pl.coderslab.user_task;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import pl.coderslab.task.Task;
+import pl.coderslab.user.User;
 import pl.coderslab.user_task_comment.UserTaskComment;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @ToString
-@Table(name = "comment")
-public class Comment implements Serializable {
+@Table
+public class UserTask implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    private String comment;
+    @ManyToOne
+    private User user;
 
-    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL)
+    @ManyToOne
+    private Task task;
+
+    private Date deadline;
+    private boolean isDone;
+
+    @OneToMany(mappedBy = "userTask", cascade = CascadeType.ALL)
     @ToString.Exclude
     private List<UserTaskComment> userTaskComments = new ArrayList<>();
 
