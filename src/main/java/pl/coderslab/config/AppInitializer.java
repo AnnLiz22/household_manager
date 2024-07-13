12642e0eaf03b40b1a3797674f3ab0b1a2efa116
AnnLiz22 <bielakam@gmail.com>
2024-07-13 21:22:32 +0200

    added on startup method, with context and dispatcher

--- src/main/java/pl/coderslab/config/AppInitializer.java
+++ src/main/java/pl/coderslab/config/AppInitializer.java
@@ -1,4 +1,30 @@
 package pl.coderslab.config;
 
-public class AppInitializer {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.FilterRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx =
+                new AnnotationConfigWebApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.setServletContext(servletContext);
+        ServletRegistration.Dynamic servlet =
+                servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+
+        FilterRegistration.Dynamic fr = servletContext.addFilter("encodingFilter",
+                new CharacterEncodingFilter());
+        fr.setInitParameter("encoding", "UTF-8");
+        fr.setInitParameter("forceEncoding", "true");
+        fr.addMappingForUrlPatterns(null, true, "/*");
+    }
 }
