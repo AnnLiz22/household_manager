4542892e3174672f7e98b6fb887990ba4a4a6b70
AnnLiz22 <bielakam@gmail.com>
2024-07-30 16:08:27 +0200

    methods added to repository

--- src/main/java/pl/coderslab/user_task/UserTaskRepository.java
+++ src/main/java/pl/coderslab/user_task/UserTaskRepository.java
@@ -1,8 +1,17 @@
 package pl.coderslab.user_task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserTaskRepository extends JpaRepository<UserTask, Long> {
+
+    @Query("select t from UserTask t where t.isDone = false")
+     List <UserTask> findByDoneIsFalse();
+
+    @Query("select t from UserTask t where t.isDone = true")
+     List <UserTask> findByDoneIsTrue(boolean isDone);
 }
